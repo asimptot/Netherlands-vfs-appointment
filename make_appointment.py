400e3358353f8c11b26c8fb8b0e287e9d4d83168
Asım <asimzorlu@gmail.com>
2022-05-22 10:15:59 +0300

    Update make_appointment.py

--- make_appointment.py
+++ make_appointment.py
@@ -6,11 +6,10 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import warnings
 from selenium.webdriver.common.keys import Keys
-import pyautogui as pg
 
 while True:
-    fromaddr = 'your_gmail'
-    password = 'your_password'
+    fromaddr = 'formaretro'
+    password = '10061144'
     try:
         p = postman(host='smtp.gmail.com', auth=(fromaddr, password))
         print("\nLogin Successful!")
@@ -18,16 +17,17 @@ while True:
     except:
         print("\nERROR: Credentials are incorrect. Please try again...")
 
+
 def sendEmail():
-    fromaddr = 'your_gmail'
-    password = 'your_password'
+    fromaddr = 'formaretro'
+    password = '10061144'
     try:
         p = postman(host='smtp.gmail.com', auth=(fromaddr, password))
         r = p.send(email(
             content=u'<p>Your Appointment is Ready</p>',
             subject='Visa Booking',
             sender='System <system@system.com>',
-            receivers=['receiver@gmail.com'],
+            receivers=['asimzorlu@gmail.com', 'melikece.tr@gmail.com'],
         ))
         assert r.ok
         time.sleep(3)
@@ -47,132 +47,114 @@ def checkAvailability():
 
     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
     driver.get(site)
+
     try:
-        time.sleep(5)
-        randevu = driver.find_element_by_id("plhMain_lnkSchApp")
-        randevu.click()
-        time.sleep(3)
+        appointment = driver.find_element_by_id("plhMain_lnkSchApp")
+        appointment.click()
         Select(driver.find_element_by_id(
             "plhMain_cboVAC")).select_by_index(2)
         driver.find_element_by_id("plhMain_btnSubmit").click()
-        time.sleep(10)
-        
+
         person = driver.find_element_by_id("plhMain_tbxNumOfApplicants")
         person.send_keys(Keys.BACKSPACE)
         person.send_keys("2")
-        time.sleep(2)
-        
+
         Select(driver.find_element_by_id(
             "plhMain_cboVisaCategory")).select_by_index(6)
         driver.find_element_by_id("plhMain_btnSubmit").click()
-        time.sleep(10)
 
         while("No date(s)" in driver.page_source):
             print(f"\nTrying Again")
             driver.refresh()
 
-
+        time.sleep(2)
         Select(driver.find_element_by_id(
             "plhMain_repAppVisaDetails_cboTitle_0")).select_by_index(1)
         time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxFName").send_keys('person1_F')
+        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxFName").send_keys('ASIM')
         time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxLName").send_keys('person1_L')
+        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxLName").send_keys('ZORLU')
         time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxContactNumber").send_keys('123123123')
+        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxContactNumber").send_keys('905455698593')
         time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxEmailAddress").send_keys('asd@def.com')
+        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxEmailAddress").send_keys('asimzorlu@gmail.com')
         time.sleep(2)
-        
+
         Select(driver.find_element_by_id(
             "plhMain_repAppVisaDetails_cboTitle_1")).select_by_index(2)
         time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl02$tbxFName").send_keys('person2_F')
+        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl02$tbxFName").send_keys('MELIKE')
         time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl02$tbxLName").send_keys('person2_L')
+        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl02$tbxLName").send_keys('ZORLU')
         time.sleep(2)
         driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl02$tbxContactNumber").send_keys(
-            '11111111')
+            '905558510909')
         time.sleep(2)
         driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl02$tbxEmailAddress").send_keys(
-            'abc@def.com')
+            'melikece.tr@gmail.com')
         time.sleep(2)
-        
-        
+
         Select(driver.find_element_by_id(
             "plhMain_cboConfirmation")).select_by_index(1)
         time.sleep(2)
         driver.find_element_by_id("plhMain_btnSubmit").click()
-        time.sleep(5)
-        
+
         while("Go to the next month" not in driver.title):
             print(f"\nTrying Again")
             driver.refresh()
-        
-        if ("Error in the application, please contact admin." in driver.page_source):
-            print(f"\nReturns Error in May.")
-            time.sleep(5)
-        elif ("No date(s) available for current month." in driver.page_source):
-            print(f"\nNo Appointments in May.")
-            time.sleep(5)
-        elif ("Appointments are" in driver.page_source):
-            print("\nAvailable in May\n")
-            sendEmail()
-            driver.find_element_by_class_name("OpenDateAllocated").click()
-            time.sleep(2)
-            driver.find_element_by_id("plhMain_gvSlot_lnkTimeSlot_0").click()
-            time.sleep(2)
-            driver.switch_to.alert.accept()
-            time.sleep(10)
-            pg.screenshot('resim.png')
-            time.sleep(10)
-        else:
-            print("Unexpected Error")
-        while(True):
 
+        while(True):
             driver.find_element_by_xpath(
                 "//*[@id='plhMain_cldAppointment']/tbody/tr[1]/td/table/tbody/tr/td[3]/a").click()
-            time.sleep(20)
+
             if ("Error in the application, please contact admin." in driver.page_source):
                 print(f"\nReturns Error in June.")
+
             elif ("No date(s) available for current month." in driver.page_source):
                 print(f"\nNo Appointments in June.")
+
             elif ("Appointments are" in driver.page_source):
-                print("\nUnexpected Error\n")
+                print("\nAvailable in June\n")
                 sendEmail()
+                time.sleep(5)
                 driver.find_element_by_class_name("OpenDateAllocated").click()
                 time.sleep(2)
                 driver.find_element_by_id("plhMain_gvSlot_lnkTimeSlot_0").click()
                 time.sleep(2)
                 driver.switch_to.alert.accept()
                 time.sleep(10)
-                pg.screenshot('resim.png')
+                driver.save_screenshot('Capture_June.png')
                 time.sleep(10)
+
             else:
                 print("Unexpected Error")
+
             driver.find_element_by_xpath(
                 "//*[@id='plhMain_cldAppointment']/tbody/tr[1]/td/table/tbody/tr/td[1]/a").click()
-            time.sleep(20)
+
             if ("Error in the application, please contact admin." in driver.page_source):
                 print(f"\nReturns Error in May.")
+
             elif ("No date(s) available for current month." in driver.page_source):
                 print(f"\nNo Appointments in May.")
+
             elif ("Appointments are" in driver.page_source):
                 print("\nAvailable in May\n")
                 sendEmail()
+                time.sleep(5)
                 driver.find_element_by_class_name("OpenDateAllocated").click()
                 time.sleep(2)
                 driver.find_element_by_id("plhMain_gvSlot_lnkTimeSlot_0").click()
                 time.sleep(2)
                 driver.switch_to.alert.accept()
                 time.sleep(10)
-                pg.screenshot('resim.png')
+                driver.save_screenshot('Capture_May.png')
                 time.sleep(10)
+
             else:
                 print("Unexpected Error")
 
     finally:
         driver.quit()
-
 
 checkAvailability()
