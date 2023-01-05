d148bf2d50b21c32c80a0c122a6b3d94b1df4ec6
Asım <asimzorlu@gmail.com>
2023-01-05 21:20:26 +0100

    Update make_appointment.py
    email send process has been fixed.

--- make_appointment.py
+++ make_appointment.py
@@ -1,61 +1,41 @@
-import time
+from time import sleep
 from selenium import webdriver
-from mailthon import postman, email
+import yagmail
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import warnings
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-while True:
-    fromaddr = 'your gmail username'
-    password = 'your password'
-    try:
-        p = postman(host='smtp.gmail.com', auth=(fromaddr, password))
-        print("\nLogin Successful!")
-        break
-    except:
-        print("\nERROR: Credentials are incorrect. Please try again...")
+fromaddr = 'your gmail usernam'
+app_password = 'your gmail token' #https://towardsdatascience.com/automate-sending-emails-with-gmail-in-python-449cc0c3c317
+to = 'receiver1@gmail.com'
 
 def sendEmail_ok():
-    fromaddr = 'your gmail username'
-    password = 'your password'
+    subject = 'Visa Booking'
+    content = ["Your Appointment is Ready", 'booked.png']
     try:
-        p = postman(host='smtp.gmail.com', auth=(fromaddr, password))
-        r = p.send(email(
-            content=u'<p>Your Appointment is Ready</p>',
-            subject='Visa Booking',
-            sender='System <system@system.com>',
-            receivers=['receiver1@gmail.com', 'receiver2@gmail.com'],
-            attachments=['booked.png']
-        ))
-        assert r.ok
-        time.sleep(3)
-
+        with yagmail.SMTP(fromaddr, app_password) as yag:
+            yag.send(to, subject, content)
+            print('Sent email successfully')
+        sleep(3)
     except:
-        print("\nLogin unsuccessful, try again.")
+        print("\nFailed to send email, try again.")
 
 def sendEmail_nok():
-    fromaddr = 'your gmail username'
-    password = 'your password'
+    subject = 'Error E-Mail'
+    content = ["You can find an image file attached.", 'unexpected_error.png']
     try:
-        p = postman(host='smtp.gmail.com', auth=(fromaddr, password))
-        r = p.send(email(
-            content=u'<p>Error E-Mail</p>',
-            subject='Visa Booking',
-            sender='System <system@system.com>',
-            receivers=['receiver1@gmail.com', 'receiver2@gmail.com'],
-            attachments=['unexpected_error.png']
-        ))
-        assert r.ok
-        time.sleep(3)
-
+        with yagmail.SMTP(fromaddr, app_password) as yag:
+            yag.send(to, subject, content)
+            print('Sent email successfully')
+        sleep(3)
     except:
-        print("\nLogin unsuccessful, try again.")
+        print("\nFailed to send email, try again.")
 
 def checkAvailability():
     print("\nChecking Availability...")
-    #uk_site = "https://www.vfsvisaonline.com/Netherlands-Global-Online-Appointment_Zone2/AppScheduling/AppWelcome.aspx?P=b0KsiJlv+LIdjKDvIvW+nLNY7GnUFdfuwQj4DXbs4vo="
     site = "https://www.vfsvisaonline.com/Netherlands-Global-Online-Appointment_Zone2/AppScheduling/AppWelcome.aspx?P=%2FiOLWQz9Vk07YwT57Xup3WWpcMKp4QcG2ApaJBrIfls%3D"
 
     warnings.filterwarnings("ignore")
@@ -67,56 +47,52 @@ def checkAvailability():
     driver.get(site)
 
     try:
-        appointment = driver.find_element_by_id("plhMain_lnkSchApp")
+        appointment = driver.find_element(By.ID, "plhMain_lnkSchApp")
         appointment.click()
-        Select(driver.find_element_by_id(
-            "plhMain_cboVAC")).select_by_index(2)
-        driver.find_element_by_id("plhMain_btnSubmit").click()
+        Select(driver.find_element(By.ID, "plhMain_cboVAC")).select_by_index(2)
+        driver.find_element(By.ID, "plhMain_btnSubmit").click()
 
-        person = driver.find_element_by_id("plhMain_tbxNumOfApplicants")
+        person = driver.find_element(By.ID, "plhMain_tbxNumOfApplicants")
         person.send_keys(Keys.BACKSPACE)
         person.send_keys("2")
 
-        Select(driver.find_element_by_id(
-            "plhMain_cboVisaCategory")).select_by_index(6)
-        driver.find_element_by_id("plhMain_btnSubmit").click()
+        Select(driver.find_element(By.ID, "plhMain_cboVisaCategory")).select_by_index(6)
+        driver.find_element(By.ID, "plhMain_btnSubmit").click()
 
         while("No date(s)" in driver.page_source):
             print(f"\nTrying Again")
             driver.refresh()
 
-        time.sleep(2)
-        Select(driver.find_element_by_id(
-            "plhMain_repAppVisaDetails_cboTitle_0")).select_by_index(1)
-        time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxFName").send_keys('FIRSTNAME1')
-        time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxLName").send_keys('SURNAME1')
-        time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxContactNumber").send_keys('PHONE NUMBER1')
-        time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl01$tbxEmailAddress").send_keys('username1@gmail.com')
-        time.sleep(2)
+        sleep(2)
+        Select(driver.find_element(By.ID, "plhMain_repAppVisaDetails_cboTitle_0")).select_by_index(1)
+        sleep(2)
+        driver.find_element(By.NAME, "ctl00$plhMain$repAppVisaDetails$ctl01$tbxFName").send_keys('FIRSTNAME1')
+        sleep(2)
+        driver.find_element(By.NAME, "ctl00$plhMain$repAppVisaDetails$ctl01$tbxLName").send_keys('SURNAME1')
+        sleep(2)
+        driver.find_element(By.NAME, "ctl00$plhMain$repAppVisaDetails$ctl01$tbxContactNumber").send_keys('PHONE NUMBER1')
+        sleep(2)
+        driver.find_element(By.NAME, "ctl00$plhMain$repAppVisaDetails$ctl01$tbxEmailAddress").send_keys('username1@gmail.com')
+        sleep(2)
 
-        Select(driver.find_element_by_id("plhMain_repAppVisaDetails_cboTitle_1")).select_by_index(2)
-        time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl02$tbxFName").send_keys('FIRSTNAME2')
-        time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl02$tbxLName").send_keys('SURNAME2')
-        time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl02$tbxContactNumber").send_keys('PHONE NUMBER2')
-        time.sleep(2)
-        driver.find_element_by_name("ctl00$plhMain$repAppVisaDetails$ctl02$tbxEmailAddress").send_keys('username2@gmail.com')
-        time.sleep(2)
+        Select(driver.find_element(By.ID, "plhMain_repAppVisaDetails_cboTitle_1")).select_by_index(2)
+        sleep(2)
+        driver.find_element(By.NAME, "ctl00$plhMain$repAppVisaDetails$ctl02$tbxFName").send_keys('FIRSTNAME2')
+        sleep(2)
+        driver.find_element(By.NAME, "ctl00$plhMain$repAppVisaDetails$ctl02$tbxLName").send_keys('SURNAME2')
+        sleep(2)
+        driver.find_element(By.NAME, "ctl00$plhMain$repAppVisaDetails$ctl02$tbxContactNumber").send_keys('PHONE NUMBER2')
+        sleep(2)
+        driver.find_element(By.NAME, "ctl00$plhMain$repAppVisaDetails$ctl02$tbxEmailAddress").send_keys('username2@gmail.com')
+        sleep(2)
 
-        Select(driver.find_element_by_id("plhMain_cboConfirmation")).select_by_index(1)
-        time.sleep(2)
-        driver.find_element_by_id("plhMain_btnSubmit").click()
-        time.sleep(30)
+        Select(driver.find_element(By.ID, "plhMain_cboConfirmation")).select_by_index(1)
+        sleep(2)
+        driver.find_element(By.ID, "plhMain_btnSubmit").click()
+        sleep(30)
 
         while(True):
-            driver.find_element_by_xpath(
-                "//*[@id='plhMain_cldAppointment']/tbody/tr[1]/td/table/tbody/tr/td[3]/a").click()
+            driver.find_element(By.XPATH, "//*[@id='plhMain_cldAppointment']/tbody/tr[1]/td/table/tbody/tr/td[3]/a").click()
 
             if ("Error in the application, please contact admin." in driver.page_source):
                 print(f"\nReturns Error in June.")
@@ -126,27 +102,26 @@ def checkAvailability():
 
             elif ("Appointments are" in driver.page_source):
                 print("\nAvailable in June\n")
-                time.sleep(5)
-                driver.find_element_by_class_name("OpenDateAllocated").click()
-                time.sleep(2)
-                driver.find_element_by_id("plhMain_gvSlot_lnkTimeSlot_0").click()
-                time.sleep(2)
+                sleep(5)
+                driver.find_element(By.CLASS_NAME, "OpenDateAllocated").click()
+                sleep(2)
+                driver.find_element(By.ID, "plhMain_gvSlot_lnkTimeSlot_0").click()
+                sleep(2)
                 driver.switch_to.alert.accept()
-                time.sleep(10)
+                sleep(10)
                 driver.save_screenshot('booked.png')
-                time.sleep(10)
+                sleep(10)
                 sendEmail_ok()
-                time.sleep(10)
+                sleep(10)
 
             else:
                 print("Unexpected Error")
                 driver.save_screenshot('unexpected_error.png')
-                time.sleep(10)
+                sleep(10)
                 sendEmail_nok()
-                time.sleep(10)
+                sleep(10)
 
-            driver.find_element_by_xpath(
-                "//*[@id='plhMain_cldAppointment']/tbody/tr[1]/td/table/tbody/tr/td[1]/a").click()
+            driver.find_element(By.XPATH, "//*[@id='plhMain_cldAppointment']/tbody/tr[1]/td/table/tbody/tr/td[1]/a").click()
 
             if ("Error in the application, please contact admin." in driver.page_source):
                 print(f"\nReturns Error in May.")
@@ -156,24 +131,24 @@ def checkAvailability():
 
             elif ("Appointments are" in driver.page_source):
                 print("\nAvailable in May\n")
-                time.sleep(5)
-                driver.find_element_by_class_name("OpenDateAllocated").click()
-                time.sleep(2)
-                driver.find_element_by_id("plhMain_gvSlot_lnkTimeSlot_0").click()
-                time.sleep(2)
+                sleep(5)
+                driver.find_element(By.CLASS_NAME, "OpenDateAllocated").click()
+                sleep(2)
+                driver.find_element(By.ID, "plhMain_gvSlot_lnkTimeSlot_0").click()
+                sleep(2)
                 driver.switch_to.alert.accept()
-                time.sleep(10)
+                sleep(10)
                 driver.save_screenshot('booked.png')
-                time.sleep(10)
+                sleep(10)
                 sendEmail_ok()
-                time.sleep(10)
+                sleep(10)
 
             else:
                 print("Unexpected Error")
                 driver.save_screenshot('unexpected_error.png')
-                time.sleep(10)
+                sleep(10)
                 sendEmail_nok()
-                time.sleep(10)
+                sleep(10)
 
     finally:
         driver.quit()
